f7863c3bc5c82c94adad91ec8c3ec362b35719dd
Rodrigo Parra Nicolau <rodri.parra13@gmail.com>
2013-09-08 12:26:04 +0200

    Superposición de imágenes
    Arreglado problema de superposición de imágenes al clickar varias veces
    en el pulsador.
    BUG: El sonido del animal no vuelve a sonar por muchas veces que se
    pulse.

--- Funciones.js
+++ Funciones.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
 /*Sonido aleatorio*/
     var numImag = Math.floor(Math.random() * (4)) + 1; 
     var contador = 0;
+    var contador2 = 0;
 
     function BienHecho() {
     
@@ -45,76 +46,79 @@ $(document).ready(function () {
 
 /*Evento hacer sonar audio*/
     $("#audio").click(function() {
+        contador2++;
 
-        
-        if (numImag == 1) {
-    	   $("#audio").append("<audio src= sonidos/gallo.mp3 autoplay='True' />"); /*Añade la etiqueta audio después de la id indicada*/
-           
-    	   $("#div4").click(function(){
-            BienHecho();
-           });
-           $("#div1").click(function(){
-            MalHecho();
-           });
-           $("#div2").click(function(){
-            MalHecho();
-           });
-           $("#div3").click(function(){
-            MalHecho();
-           });
+        if (contador2 == 1) {
 
-        };
-        if (numImag == 2) {
-            $("#audio").append("<audio src= sonidos/vaca.mp3 autoplay='True' />"); /*Añade la etiqueta audio después de la id indicada*/
-            
-            $("#div3").click(function () {
+            if (numImag == 1) {
+        	   $("#audio").append("<audio src= sonidos/gallo.mp3 autoplay='True' />"); /*Añade la etiqueta audio después de la id indicada*/
+               
+        	   $("#div4").click(function(){
                 BienHecho();
-            });
-            $("#div1").click(function(){
-            MalHecho();
-            });
-            $("#div2").click(function(){
-            MalHecho();
-            });
-            $("#div4").click(function(){
-            MalHecho();
-            });
-        };
-        if (numImag == 3) {
-            $("#audio").append("<audio src= sonidos/cerdo.mp3 autoplay='True' />"); /*Añade la etiqueta audio después de la id indicada*/
-            
-            $("#div1").click(function(){
-                BienHecho();
-            });
-            $("#div4").click(function(){
-            MalHecho();
-            });
-            $("#div2").click(function(){
-            MalHecho();
-            });
-            $("#div3").click(function(){
-            MalHecho();
-            });
-        };
-        if (numImag == 4) {
-            $("#audio").append("<audio src= sonidos/oveja.mp3 autoplay='True' />"); /*Añade la etiqueta audio después de la id indicada*/
-            
-            $("#div2").click(function(){
-                BienHecho();
-            });
-            $("#div1").click(function(){
-            MalHecho();
-            });
-            $("#div4").click(function(){
-            MalHecho();
-            });
-            $("#div3").click(function(){
-            MalHecho();
-            });
-        };
+               });
+               $("#div1").click(function(){
+                MalHecho();
+               });
+               $("#div2").click(function(){
+                MalHecho();
+               });
+               $("#div3").click(function(){
+                MalHecho();
+               });
 
-    	$("div:hidden").fadeIn(1500); /*Hace aparecer las div ocultas*/
-    	$("#contenedor").css("margin-top", "40px"); /*Propiedad CSS para que, después del click, el pulsador se mantenga en su sitio*/
+            };
+            if (numImag == 2) {
+                $("#audio").append("<audio src= sonidos/vaca.mp3 autoplay='True' />"); /*Añade la etiqueta audio después de la id indicada*/
+                
+                $("#div3").click(function () {
+                    BienHecho();
+                });
+                $("#div1").click(function(){
+                MalHecho();
+                });
+                $("#div2").click(function(){
+                MalHecho();
+                });
+                $("#div4").click(function(){
+                MalHecho();
+                });
+            };
+            if (numImag == 3) {
+                $("#audio").append("<audio src= sonidos/cerdo.mp3 autoplay='True' />"); /*Añade la etiqueta audio después de la id indicada*/
+                
+                $("#div1").click(function(){
+                    BienHecho();
+                });
+                $("#div4").click(function(){
+                MalHecho();
+                });
+                $("#div2").click(function(){
+                MalHecho();
+                });
+                $("#div3").click(function(){
+                MalHecho();
+                });
+            };
+            if (numImag == 4) {
+                $("#audio").append("<audio src= sonidos/oveja.mp3 autoplay='True' />"); /*Añade la etiqueta audio después de la id indicada*/
+                
+                $("#div2").click(function(){
+                    BienHecho();
+                });
+                $("#div1").click(function(){
+                MalHecho();
+                });
+                $("#div4").click(function(){
+                MalHecho();
+                });
+                $("#div3").click(function(){
+                MalHecho();
+                });
+            };
+
+        	$("div:hidden").fadeIn(1500); /*Hace aparecer las div ocultas*/
+        	$("#contenedor").css("margin-top", "40px"); /*Propiedad CSS para que, después del click, el pulsador se mantenga en su sitio*/
+        };
     });
 
    
